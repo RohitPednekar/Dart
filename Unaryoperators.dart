5fec8509dea7db6d2ed42040447a74ea9c679ca3
rohit pednekar <rohitpednekar3@gmail.com>
2022-11-22 10:38:19 +0530

    Add files via upload

---
+++ Unaryoperators.dart
@@ -0,0 +1,21 @@
+void main() {
+  
+  //unary operators
+  //addition
+   var a = 10;
+  a = a+10;
+  print(a);
+  //subtraction
+  var b = 20;
+  b = b - 10;
+  print(b);
+  //multiplication
+  var c = 30;
+  c = c* 30;
+  print(c);
+  //division
+  int d  = 40;
+  d = d~/40;
+  print(d);
+    
+}
